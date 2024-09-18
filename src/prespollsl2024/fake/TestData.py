a47146797631e2331edc5a5d8bd8b17a7b89f0ed
nuuuwan <nuuuwan@gmail.com>
2024-09-18 17:03:50 +0530

    Added try except

--- src/prespollsl2024/fake/TestData.py
+++ src/prespollsl2024/fake/TestData.py
@@ -67,16 +67,18 @@ class TestData:
         MAX_RETRIES = 3
         T_SLEEP_BASE = 2
         for i in range(MAX_RETRIES):
-            gig_table = GIGTable(
-                'government-elections-presidential', 'regions-ec', '2019'
-            )
-            remote_data_list = gig_table.remote_data_list
-            if remote_data_list:
-                return remote_data_list
-
-            t_sleep = T_SLEEP_BASE**i
-            log.error(f'[HACK_get_remote_data_list] Sleeping for {t_sleep}s')
-            time.sleep(t_sleep)
+            try: 
+                gig_table = GIGTable(
+                    'government-elections-presidential', 'regions-ec', '2019'
+                )
+                remote_data_list = gig_table.remote_data_list
+                if remote_data_list:
+                    return remote_data_list
+            except Exception as e:
+                log.error(f'[HACK_get_remote_data_list] {e}')
+                t_sleep = T_SLEEP_BASE**i
+                log.error(f'[HACK_get_remote_data_list] Sleeping for {t_sleep}s')
+                time.sleep(t_sleep)
         raise Exception('[HACK_get_remote_data_list] Failed')
 
     @staticmethod
