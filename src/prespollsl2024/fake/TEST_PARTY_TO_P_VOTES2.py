63562144d0f505ace2aead8e85a4f49a8152263f
nuuuwan <nuuuwan@gmail.com>
2024-09-19 21:40:06 +0530

    TEST_PARTY_TO_P_VOTES2

---
+++ src/prespollsl2024/fake/TEST_PARTY_TO_P_VOTES2.py
@@ -0,0 +1,4 @@
+TEST_PARTY_TO_P_VOTES2 = {
+    "NPP": 0.5,
+    "SJB": 0.5,
+}
--- src/prespollsl2024/fake/__init__.py
+++ src/prespollsl2024/fake/__init__.py
@@ -2,5 +2,6 @@
 
 from prespollsl2024.fake.RemoteDataUtils import RemoteDataUtils
 from prespollsl2024.fake.TEST_PARTY_TO_P_VOTES1 import TEST_PARTY_TO_P_VOTES1
+from prespollsl2024.fake.TEST_PARTY_TO_P_VOTES2 import TEST_PARTY_TO_P_VOTES2
 from prespollsl2024.fake.TestData1 import TestData1
 from prespollsl2024.fake.TestData2 import TestData2
