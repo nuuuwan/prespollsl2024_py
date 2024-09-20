79ab50fa854dae06b03e67c68381655ce89a58f0
nuuuwan <nuuuwan@gmail.com>
2024-09-20 22:35:00 +0530

    parse_ec Fri Sep 20 22:35:00 +0530 2024

--- src/prespollsl2024/__init__.py
+++ src/prespollsl2024/__init__.py
@@ -1,7 +1,7 @@
 # prespollsl2024 (auto generate by build_inits.py)
 
 from prespollsl2024.analysis import Banford, Demographics
-from prespollsl2024.app import AppPage
+# from prespollsl2024.app import AppPage
 from prespollsl2024.ec import (EDResult2, ForParty1, ForParty2, GenericResult,
                                PDResult1, Summary1, Summary2)
 from prespollsl2024.fake import (TEST_PARTY_TO_P_VOTES1,
