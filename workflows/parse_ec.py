81d9e474f42f8d26b01dce16189cf8e9b153670d
nuuuwan <nuuuwan@gmail.com>
2024-09-21 10:13:12 +0530

    new folder structure

--- workflows/parse_ec.py
+++ workflows/parse_ec.py
@@ -4,36 +4,15 @@ from utils import JSONFile, Log
 
 from prespollsl2024 import EDResult2, PDResult1
 
-log = Log("parse_data")
+log = Log("parse_ec")
 
 
 def main():
     # PDResult1
-    data_list = PDResult1.list_from_test()
-    test_path = os.path.join('data', 'ec', 'test1-2024.json')
+    data_list = PDResult1.list_from_prod()
+    test_path = os.path.join('data', 'ec', 'prod1-2024.json')
     PDResult1.store_list_to_json_compact(data_list, test_path)
 
-    # Party Data
-    party_idx = {}
-    for for_party in data_list[0].by_party:
-        party_idx[for_party.party_code] = dict(
-            party_code=for_party.party_code,
-            party_name=for_party.party_name,
-            candidate=for_party.candidate,
-        )
-    party_idx_path = os.path.join('data', 'ec', 'party_idx.json')
-    JSONFile(party_idx_path).write(party_idx)
-    log.info(f'Wrote {party_idx_path}')
-
-    party_id_list = sorted(list(party_idx.keys()))
-    party_id_list_path = os.path.join('data', 'ec', 'party_id_list.json')
-    JSONFile(party_id_list_path).write(party_id_list)
-    log.info(f'Wrote {party_id_list_path}')
-
-    # EDResult2
-    data_list = EDResult2.list_from_test()
-    test_path = os.path.join('data', 'ec', 'test2-2024.json')
-    EDResult2.store_list_to_json_compact(data_list, test_path)
 
 
 if __name__ == "__main__":
