fa9bdf677b039a986ded368b24c2a6f095001e68
nuuuwan <nuuuwan@gmail.com>
2024-09-22 06:43:47 +0530

    Update tweet

--- workflows/media/build_tweet.py
+++ workflows/media/build_tweet.py
@@ -8,8 +8,8 @@ log = Log('build_tweet')
 def main():
     election_type = 'Presidential'
     date = '2024-09-21'
-    start_n_results_display = 19
-    end_n_results_display = 19
+    start_n_results_display = 29
+    end_n_results_display = 29
 
     driver = None
     app_page = None
