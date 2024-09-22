a0420decde83202e535d99e1fe20d76e7f58eccf
nuuuwan <nuuuwan@gmail.com>
2024-09-22 16:42:22 +0530

    parse_ec.py

--- workflows/parse_ec.py
+++ workflows/parse_ec.py
@@ -2,16 +2,16 @@ import os
 
 from utils import Log
 
-from prespollsl2024.ec import PDResult1
+from prespollsl2024.ec import EDResult2
 
 log = Log("parse_ec")
 
 
 def main():
-    # PDResult1
-    data_list = PDResult1.list_from_prod()
-    test_path = os.path.join('data', 'ec', 'prod1-2024.json')
-    PDResult1.store_list_to_json_compact(data_list, test_path)
+    # EDResult2
+    data_list = EDResult2.list_from_prod()
+    test_path = os.path.join('data', 'ec', 'prod2-2024.json')
+    EDResult2.store_list_to_json_compact(data_list, test_path)
 
 
 if __name__ == "__main__":
